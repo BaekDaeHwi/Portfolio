072cf34885bf6c189bd78b67566ba3be23fea505
BaekDaeHwi <qoreognl@gmail.com>
2021-01-31 20:35:51 +0800

    js_menu toggle and project UI

--- main.js
+++ main.js
@@ -15,10 +15,10 @@ document.addEventListener('scroll',()=>{
     }
 });
 
+
 //Handle scrolling when tapping on the navbar menu
 const navbarMenu=document.querySelector('.navbar_menu');
 navbarMenu.addEventListener('click',(event)=>{
-    
     const target = event.target;
     const link = target.dataset.link;
     if(link==null){
@@ -27,8 +27,17 @@ navbarMenu.addEventListener('click',(event)=>{
     //check value in console
     console.log(event.target.dataset.link);
     
+    navbarMenu.classList.remove('open');
     scrollIntoView(link);
 });
+
+
+//Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector('.navbar_toggle-btn');
+navbarToggleBtn.addEventListener('click', ()=>{
+    navbarMenu.classList.toggle('open');
+});
+
 
 //Handle click on "Contact me" button on home
 const homeContactBtn = document.querySelector('.home_contact');
